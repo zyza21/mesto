a9d29c8ed014f5ac643cdd29513c46f180dce626
Julia Maslova <zyza21@list.ru>
2020-11-29 00:12:14 +0300

    initial

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let name = document.querySelector('.profile__name');
+let infoAbout = document.querySelector('.profile__info-about');
+let inputName = document.querySelector('.input__text_type_name');
+let inputInfoAbout = document.querySelector('.input__text_type_about');
+let submitButton = document.querySelector('.popup__submit-button');
+let closeButton = document.querySelector('.popup__close-button');
+
+function openPopup() {
+  popup.classList.add('popup_active');
+  inputName.value = name.textContent;
+  inputInfoAbout.value = infoAbout.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_active');
+}
+
+
+function editProfile() {
+  name.textContent = `${inputName.value}`;
+  infoAbout.textContent = `${inputInfoAbout.value}`;
+  closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+submitButton.addEventListener('click', editProfile);
+closeButton.addEventListener('click', closePopup);
+
+

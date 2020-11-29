fe652de8cc5c2195347c66d6704958e4fda9aaa0
Julia Maslova <zyza21@list.ru>
2020-11-29 21:00:15 +0300

    initial

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,32 @@
 let popup = document.querySelector('.popup');
+let formElement = popup.querySelector('.popup__in-container');
 let editButton = document.querySelector('.profile__edit-button');
 let name = document.querySelector('.profile__name');
 let infoAbout = document.querySelector('.profile__info-about');
-let inputName = document.querySelector('.popup__input-name');
-let inputInfoAbout = document.querySelector('.popup__input-about');
-let submitButton = document.querySelector('.popup__submit-button');
+let inputName = formElement.querySelector('.popup__input-name');
+let inputInfoAbout = formElement.querySelector('.popup__input-about');
+let submitButton = formElement.querySelector('.popup__submit-button');
 let closeButton = document.querySelector('.popup__close-button');
 
 function openPopup() {
-  popup.classList.add('popup_active');
   inputName.value = name.textContent;
   inputInfoAbout.value = infoAbout.textContent;
+  popup.classList.add('popup_active'); 
 }
 
 function closePopup() {
   popup.classList.remove('popup_active');
 }
 
-
-function editProfile() {
-  name.textContent = `${inputName.value}`;
-  infoAbout.textContent = `${inputInfoAbout.value}`;
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  name.textContent = inputName.value; 
+  infoAbout.textContent = inputInfoAbout.value;
   closePopup();
 }
 
 editButton.addEventListener('click', openPopup);
-submitButton.addEventListener('click', editProfile);
 closeButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler); 
 
 
